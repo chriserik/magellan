d7a114729d3f9fdd36f27578fbc2c0d626b23c97
Christian Schultheiss <christian@cschultheiss.de>
2013-09-13 11:56:36 +0200

    added rails base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magellan::Application.config.secret_key_base = 'e648c5f88c48148b6bb72a777d58d2d2b6a18720a20d285d0ca5a39edd9749ac403dd72bb41ec3d03325df355ddcd2b97ac59e284b1f0be2930c771c805f8466'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Magellan::Application.config.session_store :cookie_store, key: '_magellan_session'
